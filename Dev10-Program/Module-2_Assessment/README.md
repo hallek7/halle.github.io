87c567fe83c1ff4e38ce5d75bff3dd55f0a4fdc4
hallek7 <kebe0010@umn.edu>
2020-04-25 12:24:57 -0500

    more updates

--- Dev10-Program/Module-2_Assessment/README.md
+++ Dev10-Program/Module-2_Assessment/README.md
@@ -1,2 +1,8 @@
-the theme of this game is to guess a country name 
-Used  key events to listen for the letters that      players will type. 
+1. The theme of this game is to guess a country name 
+2. Used javascript and jquery to create this app
+3. Used  key events to listen for the letters that  players will type. 
+4. Player has 8 chances to guess the correct word 
+5. The appt tracks the # of Wins and losses  as well as 
+Number of guesses remaining  using incrment and decrement method 
+6. When the user hits the same letter more than once , there will be no  output and  it doesn't count aganist the # of guesses left.
+7. After the user wins/loses the game, the app   reloads and  another word is randomly pickd for the user to guess. 
--- Dev10-Program/Module-2_Assessment/assets/javascript/game.js
+++ Dev10-Program/Module-2_Assessment/assets/javascript/game.js
@@ -1,16 +1,15 @@
 // set up variables 
-var countryNames = ["Mexico", "US", "Egypt", "Ethiopia", "Jordan", "Haiti", "Canada", "Morocco", "England", "South Africa"];
+var countryNames = ["Mexico", "US", "Egypt", "Ethiopia", "Jordan", "Haiti", "Canada", "Morocco", "England", "France", "Italy", "Turkey"];
 var totalGuesses = 8; // number of tries
 var randomPick; // array number the machine choose randomly
 var wordGuessed = []; // This will be the word we actually build to match the current word
 var allGuesses = [];
-// var wordGuess = "";
 var guessLeft = 8; // How many tries the player has left
 var finishedGame = false; // Flag for 'press any key to try again'     
 var wins = 0; // # of wins
 var losses = 0; //# of losses
 
-//
+
 function refreshScreen() {
     document.getElementById("gameWins").innerHTML = wins;
     document.getElementById("gameLosses").innerHTML = losses;
@@ -22,32 +21,26 @@ function refreshScreen() {
         wordGuess += wordGuessed[i];
     }
 
-    //update guesses, word, and letters entered
+    //update guesses, word, and letters entered by joining the letters 
     document.getElementById("currentWord").innerHTML = wordGuessed.join('');
     document.getElementById("guessLeft").innerHTML = guessLeft;
     document.getElementById("userGuess").innerHTML = allGuesses.join(' , ');
 };
 
-function restartGame() {
-    countryNames = ["Mexico", "US", "Egypt", "Ethiopia", "Jordan", "Haiti", "Canada", "Morocco", "England", "South Africa"];
+function restartGame() { //after each win/loss 
+    countryNames = ["Mexico", "US", "Egypt", "Ethiopia", "Jordan", "Haiti", "Canada", "Morocco", "England", "South Africa", "France", "Italy", "Turkey"];
     totalGuesses = 8; // number of tries
-    // var userGuess = []; // letters the user guessed
     wordGuessed = []; // This will be the word we actually build to match the current word
     allGuesses = [];
-    // var wordGuess = "";
     guessLeft = 8; // How many tries the player has left
     finishedGame = false; // Flag for 'press any key to try again'   
     startGame()
 
-}
+};
 // start the game
 function startGame() {
-    // $(document).keypress(function(event) {
-
-    //     refreshScreen();
-    //})
+    //guess letter 
     function makeGuess(letter) {
-
         var actual = countryNames[randomPick]
         var result = wordGuessed
         var foundLetter = false;
@@ -68,7 +61,7 @@ function startGame() {
                     result[i] = event.key.toLowerCase()
                 }
             }
-        }
+        };
 
         if (!foundLetter) {
             if (guessLeft === 0) {
@@ -81,7 +74,7 @@ function startGame() {
 
 
 
-    }
+    };
 
 
     guessLeft = totalGuesses;
@@ -91,32 +84,37 @@ function startGame() {
 
     //query the table data images 
     if (countryNames[randomPick] == countryNames[0]) {
-        $('.hint').html("<img src='./assets/images/SoCal.jpg' width='300'/>");
+        $('.hint').html("<img src='./assets/images/SoCal.jpg'/>");
 
     } else if (countryNames[randomPick] == countryNames[1]) {
-        $('.hint').html("<img src='./assets/images/hiWay.jpg' width='300'/>");
+        $('.hint').html("<img src='./assets/images/hiWay.jpg'/>");
 
     } else if (countryNames[randomPick] == countryNames[2]) {
-        $('.hint').html("<img src='./assets/images/LA.jpg' width='300'/>");
+        $('.hint').html("<img src='./assets/images/LA.jpg'/>");
 
     } else if (countryNames[randomPick] == countryNames[3]) {
-        $('.hint').html("<img src='./assets/images/car.jpg' width='300'/>");
+        $('.hint').html("<img src='./assets/images/car.jpg'/>");
     } else if (countryNames[randomPick] == countryNames[4]) {
-        $('.hint').html("<img src='./assets/images/sky.jpg' width='300'/>")
+        $('.hint').html("<img src='./assets/images/sky.jpg'/>")
 
     } else if (countryNames[randomPick] == countryNames[5]) {
-        $('.hint').html("<img src='./assets/images/utah.jpg' width='300'/>");
+        $('.hint').html("<img src='./assets/images/utah.jpg'/>");
 
     } else if (countryNames[randomPick] == countryNames[6]) {
-        $('.hint').html("<img src='./assets/images/house.jpg' width='300'/>");
+        $('.hint').html("<img src='./assets/images/house.jpg'/>");
 
     } else if (countryNames[randomPick] == countryNames[7]) {
-        $('.hint').html("<img src='./assets/images/egypt.jpg' width='300'/>");
+        $('.hint').html("<img src='./assets/images/egypt.jpg'/>");
     } else if (countryNames[randomPick] == countryNames[8]) {
-        $('.hint').html("<img src='./assets/images/shore.jpg' width='300'/>");
+        $('.hint').html("<img src='./assets/images/shore.jpg'/>");
 
     } else if (countryNames[randomPick] == countryNames[9]) {
-        $('.hint').html("<img src='./assets/images/homes.jpg' width='300'/>");
+        $('.hint').html("<img src='./assets/images/homes.jpg'/>");
+
+    } else if (countryNames[randomPick] == countryNames[9]) {
+        $('.hint').html("<img src='./assets/images/ethi.jpg'/>");
+
+
 
     } else {
         $('.hint').text('None of The Above');
@@ -209,4 +207,4 @@ function startGame() {
             }
         }
     };
-}; //close main function
+}; //close start function
